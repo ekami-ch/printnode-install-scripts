74874b89043aa9f8291d10e61898cc7a5d6ba646
yurihikari <quangminhjean@gmail.com>
2024-04-22 19:12:35 +0200

    Using params instead of interactive script

--- install.bash
+++ install.bash
@@ -7,126 +7,132 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-# PrintNode download links
-printnode_links=(
-  https://dl.printnode.com/client/printnode/4.27.8/PrintNode-4.27.8-pi-bullseye-aarch64.tar.gz
-  https://dl.printnode.com/client/printnode/4.27.8/PrintNode-4.27.8-pi-bullseye-aarch64.tar.gz
-  https://dl.printnode.com/client/printnode/4.27.17/PrintNode-4.27.17-ubuntu-22.04-x86_64.tar.gz
-)
+# 3 arguments:
+#  - architecture
+#  - distribution
+#  - PrintNode API key
 
-# PrintNode package names
-printnode_packages=(
-  PrintNode-4.27.8-pi-bullseye-aarch64.tar.gz
-  PrintNode-4.27.8-pi-bullseye-aarch64.tar.gz
-  PrintNode-4.27.17-ubuntu-22.04-x86_64.tar.gz
-)
+# Usage:
+# ./install.bash <architecture> <distribution> <PrintNode client key>
+echo "PrintNode Setup"
+echo "Checking the arguments"
+if [ $# -ne 3 ]; then
+  echo "Invalid number of arguments. Usage: ./install.bash <architecture> <distribution> <PrintNode API key>"
+  exit 1
+fi
 
-printnode_editions=(
-  pi-bullseye-aarch64
-  pi-bookworm-aarch64
-  ubuntu-22.04-x86_64
-)
+# PrintNode architecture
+architecture=$1
+# PrintNode distribution
+distribution=$2
+# PrintNode API key
+api_key=$3
 
-# Menu 
-echo "PrintNode Setup Menu"
-echo "What do you want to do?"
-echo "1. Install PrintNode for Raspbian Bullseye aarch64 (Raspberry pi 4 & 4B)"
-echo "2. Install PrintNode for Raspbian Bookworm aarch64 (Raspberry pi 5)"
-echo "3. Install PrintNode for Ubuntu 22.04 LTS AMD64"
-echo "4. Exit"
-read -p "Enter your choice [1-4]: " choice
 
-# Switch case to set printnode_link and printnode_package
-case $choice in
-    1)
-        printnode_link=${printnode_links[0]}
-        printnode_package=${printnode_packages[0]}
-        printnode_edition=${printnode_editions[0]}
-        ;;
-    2)
-        printnode_link=${printnode_links[1]}
-        printnode_package=${printnode_packages[1]}
-        printnode_edition=${printnode_editions[1]}
-        ;;
-    3)
-        printnode_link=${printnode_links[2]}
-        printnode_package=${printnode_packages[2]}
-        printnode_edition=${printnode_editions[2]}
-        ;;
-    4)
-        echo "Exiting..."
-        exit 0
-        ;;
-    *)
-        echo "Invalid choice. Please enter a number between 1 and 4."
-        exit 1
-        ;;
-esac
+# Declare an associative array to hold the download links
+declare -A printnode_links
+printnode_links["aarch64,pi-bullseye"]="https://dl.printnode.com/client/printnode/4.27.8/PrintNode-4.27.8-pi-bullseye-aarch64.tar.gz"
+printnode_links["aarch64,pi-bookworm"]="https://dl.printnode.com/client/printnode/4.27.8/PrintNode-4.27.8-pi-bookworm-aarch64.tar.gz"
+printnode_links["x86_64,ubuntu-22.04"]="https://dl.printnode.com/client/printnode/4.27.17/PrintNode-4.27.17-ubuntu-22.04-x86_64.tar.gz"
 
-echo "Selected PrintNode Edition: $printnode_edition"
-sleep 2
+# Check if the provided architecture and distribution are valid
+printnode_edition="${architecture},${distribution}"
+if [ -z "${printnode_links[$printnode_edition]}" ]; then
+    echo "Invalid architecture or distribution. Valid combinations are:"
+    for k in "${!printnode_links[@]}"; do
+        echo " - $k"
+    done
+    exit 1
+fi
+
+# PrintNode download link
+printnode_link=${printnode_links[$printnode_edition]}
+
+echo "Download link: $printnode_link"
+
+# Execute the curl command with an empty password and store its output in a variable
+echo "Getting the client key from PrintNode API"
+
+
+# Making the API call and storing the result along with the HTTP status code
+response=$(curl -s -w "\n%{http_code}" "https://api.printnode.com/client/key/0?version=4.7.1&edition=printnode" -u "$api_key:")
+client_key=$(echo "$response" | head -n -1)
+http_status=$(echo "$response" | tail -n 1)
+
+# Check if the curl command succeeded and valid HTTP status code received
+if [ "$http_status" -ne 200 ]; then
+    echo "Failed to retrieve client key due to a network or server error. Status code: $http_status"
+    echo "Response was: $client_key"
+    exit 1
+fi
+
+# Simple error handling with grep
+if echo "$client_key" | grep -q "API Key not found"; then
+    echo "API Key error: $client_key"
+    exit 1
+fi
+
+# Assume key retrieval was successful if no errors were found
+echo "Client key successfully retrieved: $client_key"
+
+# Remove quotes from the client_key variable
+echo "Removing quotes from the client key"
+client_key=$(echo "$client_key" | tr -d '"')
+echo "Client key quotes removed: $client_key"
+
+# Verify that client_key is not empty and does not contain only spaces
+if [[ -z $client_key || $client_key =~ ^[[:space:]]*$ ]]; then
+    echo "No client key was retrieved, or the key is invalid."
+    exit 1
+else
+    echo "Client key successfully retrieved and validated."
+fi
+
+
 
 # PrintNode Setup
 echo "Setting up PrintNode"
-## Switch case printnode_edition
+## Specific dependencies for printnode_edition
 case $printnode_edition in
-    pi-bullseye-aarch64)
-        ;;
-    pi-bookworm-aarch64)
+    pi-bookworm,aarch64)
         ## Installing missing dependencies
         echo "Installing missing dependencies"
         echo "Installing libssl1.1"
-        apt-get install libssl1.1
+        apt-get install -y libssl1.1
         echo "libssl1.1 installed"
         echo "Missing dependencies installed"
         ;;
-    ubuntu-22.04-x86_64)
-        ;;
-    *) 
-        echo "Invalid PrintNode Edition. Exiting..."
-        exit 1
-        ;;
 esac
+
+
 ## Download PrintNode
 echo "Downloading PrintNode"
-wget $printnode_link
+# If temp file exists, remove it
+if [ -f /tmp/printnode.tar.gz ]; then
+    rm -f /tmp/printnode.tar.gz
+fi 
+
+wget $printnode_link -O /tmp/printnode.tar.gz
 echo "PrintNode downloaded"
+
 ## Create PrintNode directory in /usr/local
 echo "Creating PrintNode directory in /usr/local"
 mkdir /usr/local/PrintNode
 echo "/usr/local/PrintNode created"
+
 ## Extract PrintNode in the PrintNode directory
 echo "Extracting PrintNode in the PrintNode directory"
-tar xf $printnode_package -C /usr/local/PrintNode --strip-components=1
+tar xf /tmp/printnode.tar.gz -C /usr/local/PrintNode --strip-components=1
 echo "PrintNode extracted"
+
 ## Setup the autostart 
 echo "Copying init.sh to /etc/init.d/PrintNode"
 cp /usr/local/PrintNode/init.sh /etc/init.d/PrintNode
 echo "init.sh copied to /etc/init.d/PrintNode"
-## Ask the user input for a printnode api key. Show them the api key and ask if it is correct, if not ask them again.
-read -p "Enter your PrintNode API key: " api_key
-### try to curl "https://api.printnode.com/client/key/0?version=4.7.1&edition=printnode" -u XXXX_API_KEY_XXXX, if it fails ask them again
-### if it succeeds break the loop
-while true; do
-    echo "Checking the API key"
-    if curl "https://api.printnode.com/client/key/0?version=4.7.1&edition=printnode" -u $api_key:''; then
-        echo "API key is correct"
-        break
-    else
-        echo "API key is incorrect"
-        read -p "Enter your PrintNode API key: " api_key
-    fi
-done
+
+
 ### Add the curl result to the /etc/init.d/PrintNode script at the line cmd="/usr/local/PrintNode/PrintNode --headless --shutdown-on-sigint --web-interface --remove-scales-support"
 echo "Adding the key to /etc/init.d/PrintNode"
-### Execute the curl command with an empty password and store its output in a variable
-echo "Getting the client key"
-client_key=$(curl -s "https://api.printnode.com/client/key/0?version=4.7.1&edition=printnode" -u $api_key:)
-echo "Client key retrieved"
-### Remove quotes from the client_key variable
-echo "Removing quotes from the client key"
-client_key=$(echo $client_key | tr -d '"')
-echo "Client key quotes removed"
 ### Use sed to replace the placeholder with the actual client key
 echo "Adding key to /etc/init.d/PrintNode"
 sed -i 's@\(cmd="/usr/local/PrintNode/PrintNode --headless --shutdown-on-sigint --web-interface --remove-scales-support\).*@\1'" --client-key=$client_key\""'@' /etc/init.d/PrintNode
@@ -171,7 +177,7 @@ echo "You can install printer drivers manually safely now."
 
 # Cleanup
 echo "Cleaning up"
-rm $printnode_package
+rm /tmp/printnode.tar.gz
 echo "Cleanup complete"
 
 echo "Setup complete. Please setup your printers and RustDesk manually before restarting your Raspberry Pi."
